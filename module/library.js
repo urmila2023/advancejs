21f5a7e89ab5ccfcbd05e1a7db32f43770b43a53
urmila2023 <urmila@difm.tech>
2023-05-19 01:46:01 -0400

    change

---
+++ module/library.js
@@ -0,0 +1,34 @@
+// export let message = "ES6 Module";
+// export function user(name){
+//     console.log(`Hello ${name}`);
+// }
+// export class test{
+//     constructor(){
+//         console.log("Constructor Method");
+//     }
+// }
+
+
+
+//  let message = "ES6 Module";
+//  function user(name){
+//     console.log(`Hello ${name}`);
+// }
+// class test{
+//     constructor(){
+//         console.log("Constructor Method");
+//     }
+// }
+// export{message, user, test};
+
+
+
+// export default function(){
+//     console.log("Urmila Maurya");
+// }
+
+
+function user(){
+    return `Hiii`;
+}
+export {user};
---
+++ module/main.js
@@ -0,0 +1,26 @@
+// import { message , user, test } from "./library.js";
+// console.log(message);
+// // document.body.innerHTML = message;
+// user("Urmila Maurya");
+// let a = new test();
+
+
+// import * as all from "./library.js";
+// console.log(all.message);
+// // document.body.innerHTML = message;
+// all.user("Urmila Maurya");
+// let a = new all.test();
+
+
+
+// import {default as aaa } from "./library.js";
+// aaa();
+
+
+// import aaa from "./library.js";
+// aaa();
+
+
+
+import {user } from "./bridge.js";
+console.log(user());
